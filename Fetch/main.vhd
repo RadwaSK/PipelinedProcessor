978d833b58855facb046c41214ce0fdf2c189892
AbdelrahmanBadr97 <elnagmy45@gmail.com>
2020-05-11 16:57:02 +0200

    fetch_stage

---
+++ Fetch/main.vhd
@@ -0,0 +1,54 @@
+LIBRARY IEEE;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE IEEE.std_logic_1164.all;
+
+ENTITY CPU_main IS
+PORT( Clk : IN std_logic ) ; 
+
+END CPU_main;
+
+
+ARCHITECTURE a_CPU_main OF CPU_main IS
+
+COMPONENT ram IS
+PORT (clk : IN std_logic;
+--we : IN std_logic;
+re : IN std_logic;
+addr : IN std_logic_vector (31 DOWNTO 0); 
+--datain : IN std_logic_vector(15 DOWNTO 0);
+dataout : OUT std_logic_vector(15 DOWNTO 0); 
+PCRest : OUT std_logic_vector(31 DOWNTO 0); 
+PCINT : OUT std_logic_vector(31 DOWNTO 0)); 
+end COMPONENT ; 
+
+COMPONENT HDU IS
+port ( RdstALU, RdstDec :   in std_logic_vector(2 downto 0);
+RamInp :   in std_logic_vector(15 downto 0);
+IRregin : in std_logic_vector (1 downto 0);
+Asel, Bsel  :       out std_logic_vector (1 downto 0);
+OpFlagBeforeALU, Stall    :   out std_logic);
+end COMPONENT ; 
+
+COMPONENT FetMpd IS 
+PORT( Clk , stall : IN std_logic;
+  IRFlag: IN std_logic_vector(1 DOWNTO 0);
+  outRam: in std_logic_vector(15 DOWNTO 0);
+  RegDest , MemPC ,PCRest , PCINT: IN std_logic_vector(31 DOWNTO 0); 
+  IR : inOUT std_logic_vector(15 DOWNTO 0);
+  JZStates  : inOUT std_logic_vector(1 DOWNTO 0); -- 00 intial value  01 waiting for validation  10 you are correct  11 you need to flush
+  PC  : OUT std_logic_vector(31 DOWNTO 0);
+  PCout : inOUT std_logic_vector(31 DOWNTO 0);
+  Regdecoder : OUT std_logic_vector(2 DOWNTO 0)) ; -- to get red Dest at call and jmp 
+end COMPONENT ; 
+
+signal RegDest , MemPC , PCINT , PCRest , PCout , PC: std_logic_vector(31 DOWNTO 0);
+signal RamOut , IR: std_logic_vector(15 DOWNTO 0);
+signal RdstALU, RdstDec , Regdecoder: std_logic_vector(2 DOWNTO 0);
+signal IRregin , Asel, Bsel , JZStates : std_logic_vector(1 DOWNTO 0);
+signal Stall , OpFlagBeforeALU: std_logic;
+begin 
+
+Memory : ram port map (Clk , '1' , PCout , RamOut , PCINT , PCRest);
+Fetch : FetMpd port map (Clk , Stall , IRregin , RamOut , RegDest , MemPC , PCRest , PCINT , IR , JZStates , PC , PCout ,Regdecoder );
+Hazard_Detection : HDU port map (RdstALU, RdstDec , RamOut , IRregin , Asel, Bsel , OpFlagBeforeALU, Stall);
+end a_CPU_main ;
---
+++ test1.asm
@@ -0,0 +1,43 @@
+#this is a comment 
+
+.ORG 0
+10
+.ORG 2
+1A
+
+#this program has no logical function 
+.ORG 10
+INC R0 #try comment beside code
+DEC R1
+NOP
+NOT R5
+OUT R2
+CALL R4
+JMP R6
+IN R7
+JZ R0
+RET
+RTI
+NOP
+NOP
+Interrupt
+
+SWAP R0 ,R1
+#please consider the spaces or it will be an error
+ADD R5 ,R6 ,R1
+SUB R5 ,R6 ,R1
+AND R5 ,R7 ,R1
+OR R0 ,R2 ,R6
+
+
+IADD R5 ,R6 ,5
+SHL R4 ,4
+SHR R2 ,6
+
+
+PUSH R0
+POP R1
+LDM R2 ,30
+
+LDD R6 ,10021
+STD R3 ,110C0
